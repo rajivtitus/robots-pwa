44f5c247ea6ca17e4da5dcc729d86a1473b17945
Rajiv Titus <75184965+rajivtitus@users.noreply.github.com>
2022-09-09 12:01:06 -0700

    pwa initial changes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunkMiddleware from "redux-thunk";
+import "tachyons";
 
+import App from "./containers/App";
+import { requestRobots, searchRobots } from "./reducers";
 
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-import { requestRobots, searchRobots } from './reducers'
+import "./index.css";
 
-import './index.css';
+const rootReducers = combineReducers({ requestRobots, searchRobots });
 
-const logger = createLogger() 
-
-const rootReducers = combineReducers({requestRobots, searchRobots})
-
-const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
 );
-registerServiceWorker();
